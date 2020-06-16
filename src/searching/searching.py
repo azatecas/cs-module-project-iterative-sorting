11469414570f3e05ecdafb70603284f26298e0a8
Luis PM <luisp237@gmail.com>
2020-06-16 16:10:27 -0400

    completed serching.py added floor division operant to find list index

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if target == arr[i]:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,17 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    min = 0
+    max = len(arr) - 1 #larget index in arr
+
+    while min <= max:
+        mid = (min + max) // 2 #floor division
+        if arr[mid] == target:
+            return mid
+        elif target > arr[mid]:
+            min = mid + 1
+        else:
+            max = mid - 1
 
 
-    return -1  # not found
+    return - 1  # not found
